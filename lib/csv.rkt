10f5d3ac11a59f882e3e2ba29daa1d9f97553589
Dale Vaillancourt <dalev@ccs.neu.edu>
2011-03-04 20:53:27 -0500

    non-blocking io with byte strings

--- lib/csv.rkt
+++ lib/csv.rkt
@@ -1,12 +1,12 @@
 #lang racket
 (require (planet jaymccarthy/sqlite)
-         (planet dalev/core/lib/string)
          racket/generator)
 
 (provide/contract
   [in-raw-csv ((input-port?) (#:reverse? boolean? #:separator char?) . ->* . sequence?)])
 
-(define (record-separator? c) (char=? c #\newline))
+(define byte-double-quote (char->integer #\"))
+(define (double-quote? byte) (eq? byte byte-double-quote))
 
 ;; CR dalev: implement a nicer CSV interface that represents records as maps,
 ;;           allow a header to be supplied / used as override
@@ -17,56 +17,70 @@
 
   (define rev (if reverse? values reverse))
 
-  (define field-buffer (make-string 1024 #\space))
+  (define field-buffer (make-bytes 1024))
 
   (define (buffer! buf-idx c) 
-    (string-set! field-buffer buf-idx c))
+    (bytes-set! field-buffer buf-idx c))
 
   (define (emit-field! buf-idx)
-    (substring field-buffer 0 buf-idx))
+    (bytes->string/locale (subbytes field-buffer 0 buf-idx)))
 
   (define (yield! csv-fields buf-idx)
     (yield (rev (cons (emit-field! buf-idx) csv-fields)))
     (values 'start-field '() 0))
 
+  (define input-bytes (make-bytes (* 4 1024)))
+
+  (define byte-field-separator (char->integer separator))
+  (define byte-record-separator (char->integer #\newline))
+
+  (define (field-sep? byte) (eq? byte byte-field-separator))
+  (define (record-sep? byte) (eq? byte byte-record-separator))
+
   (in-generator
-    (for/fold ([state 'start-field]
+    (let loop ([state 'start-field]
                [csv-fields '()]
                [buf-idx 0])
-              ([c (in-input-port-chars in)])
-      (case state
-        [(in-field) 
-         (cond [(char=? c separator) 
-                (values 'start-field (cons (emit-field! buf-idx) csv-fields) 0)]
-               [(record-separator? c) 
-                (yield! csv-fields buf-idx)]
-               [else (buffer! buf-idx c)
-                     (values 'in-field csv-fields (add1 buf-idx))])]
-        [(start-field) 
-         (cond [(char=? separator c) 
-                (values 'start-field (cons (emit-field! buf-idx) csv-fields) 0)]
-               [(char=? #\" c)
-                (values 'in-quoted-field csv-fields buf-idx)]
-               [(record-separator? c) 
-                (yield! csv-fields buf-idx)]
-               [else 
-                 (buffer! buf-idx c)
-                 (values 'in-field csv-fields (add1 buf-idx))])]
-        [(in-quoted-field) 
-         (cond [(char=? c #\")
-                (values 'in-quoted-field-after-quote csv-fields buf-idx)]
-               [else (buffer! buf-idx c)
-                     (values 'in-quoted-field csv-fields (add1 buf-idx))])]
-        [(in-quoted-field-after-quote) 
-         (cond [(char=? c #\")
-                (buffer! buf-idx c)
-                (values 'in-quoted-field csv-fields (add1 buf-idx))]
-               [(char=? separator c)
-                (values 'start-field (cons (emit-field! buf-idx) csv-fields) 0)]
-               [(record-separator? c) 
-                (yield! csv-fields buf-idx)]
-               [(raise (exn:fail "Expected double-quote or separator"
-                                 (current-continuation-marks)))])]))))
-                
-        
-
+      (define avail (read-bytes-avail! input-bytes in))
+      (cond
+        [(number? avail) 
+         (define-values (new-state new-csv-fields new-buf-idx)
+           (for/fold ([state state] [csv-fields csv-fields] [buf-idx buf-idx])
+             ([i (in-range 0 avail)]
+              [c (in-bytes input-bytes)])
+             (case state
+               [(in-field) 
+                (cond [(field-sep? c) 
+                       (values 'start-field (cons (emit-field! buf-idx) csv-fields) 0)]
+                      [(record-sep? c) 
+                       (yield! csv-fields buf-idx)]
+                      [else (buffer! buf-idx c)
+                            (values 'in-field csv-fields (add1 buf-idx))])]
+               [(start-field) 
+                (cond [(field-sep? c) 
+                       (values 'start-field (cons (emit-field! buf-idx) csv-fields) 0)]
+                      [(double-quote? c)
+                       (values 'in-quoted-field csv-fields buf-idx)]
+                      [(record-sep? c) 
+                       (yield! csv-fields buf-idx)]
+                      [else 
+                        (buffer! buf-idx c)
+                        (values 'in-field csv-fields (add1 buf-idx))])]
+               [(in-quoted-field) 
+                (cond [(double-quote? c)
+                       (values 'in-quoted-field-after-quote csv-fields buf-idx)]
+                      [else (buffer! buf-idx c)
+                            (values 'in-quoted-field csv-fields (add1 buf-idx))])]
+               [(in-quoted-field-after-quote) 
+                (cond [(double-quote? c)
+                       (buffer! buf-idx c)
+                       (values 'in-quoted-field csv-fields (add1 buf-idx))]
+                      [(field-sep? c)
+                       (values 'start-field (cons (emit-field! buf-idx) csv-fields) 0)]
+                      [(record-sep? c) 
+                       (yield! csv-fields buf-idx)]
+                      [(raise (exn:fail "Expected double-quote or separator"
+                                        (current-continuation-marks)))])])))
+         (loop new-state new-csv-fields new-buf-idx)]
+        [(eof-object? avail) (void)]
+        [(procedure? avail) (error 'in-csv "read-bytes-avail! returned a procedure")]))))
