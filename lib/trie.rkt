e4fa6ac54f117317a77a1ac6f0b438c41963b36e
Dale Vaillancourt <dalev@ccs.neu.edu>
2011-07-14 22:31:34 -0400

    in-trie

--- lib/trie.rkt
+++ lib/trie.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 (require racket/contract
-         racket/match)
+         racket/match
+         racket/generator)
 
 (define nonempty-string/c
   (flat-named-contract 'nonempty-string/c
@@ -13,13 +14,42 @@
 ;; CR dalev: there's actually no reason to restrict to strings; any sequence
 ;; will do if we just remove uses of in-string and favor generic iterators.
 ;; I wonder what the performance difference will be on /usr/share/dict/words.
+;; One difficulty though will be `in-trie':  it needs to pick a concrete
+;; sequence type to return.  Maybe vectors are good enough for the general
+;; case.  Could provide a specialization for strings.
 (provide/contract
   [create (-> trie/c)]
   [insert! (trie/c nonempty-string/c . -> . trie/c)]
   [contains? (trie/c nonempty-string/c . -> . boolean?)]
+  [in-trie (trie/c . -> . sequence?)]
   [of-file (() (#:path path?) . ->* . trie/c)])
 
-;; CR dalev: implement the sequence interface
+(define (in-trie t)
+  (define buffer (make-string 256))
+  (define idx 0)
+  (define (current-string) (substring buffer 0 idx))
+
+  (define (double-buffer!)
+    (let* ([current-length (string-length buffer)]
+           [new-buffer (make-string (* 2 current-length))])
+      (string-copy! new-buffer 0 buffer)
+      (set! buffer new-buffer)))
+
+  (define (push! c) 
+    (string-set! buffer idx c) 
+    (set! idx (add1 idx))
+    (when (>= idx (string-length buffer))
+      (double-buffer!)))
+
+  (define (pop!) (set! idx (sub1 idx)) (string-ref buffer idx))
+
+  (in-generator
+    (let loop ([t t])
+      (for ([(char rhs) (in-hash t)])
+        (push! char)
+        (when (rhs-in-trie? rhs) (yield (current-string)))
+        (loop (rhs-subtrie rhs))
+        (pop!)))))
 
 (define (create) (make-hasheqv))
 
