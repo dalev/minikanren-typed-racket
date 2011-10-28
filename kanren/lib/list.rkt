6519521717ce1fe10abada88bb6fedbc6888a405
Dale Vaillancourt <dalev@ccs.neu.edu>
2011-10-27 21:46:10 -0400

    clean up

--- kanren/lib/list.rkt
+++ kanren/lib/list.rkt
@@ -3,66 +3,45 @@
 
 (provide (all-defined-out))
 
-(define %car
-  (lambda (p a)
-    (exist (d)
-      (== (cons a d) p))))
+(define (%car p a) (exist (d) (== (cons a d) p)))
+(define (%cdr p d) (exist (a) (== (cons a d) p)))
+(define (%cons a d p) (== (cons a d) p))
+(define (%null x) (== '() x))
+(define (%pair p) (exist (a d) (%cons a d p)))
 
-(define %cdr
-  (lambda (p d)
-    (exist (a)
-      (== (cons a d) p))))
+(define (%list l)
+  (conde
+    [(%null l) succeed]
+    [(%pair l)
+      (exist (d)
+        (%cdr l d)
+        (%list d))]
+    [else fail]))
 
-(define %cons
-  (lambda (a d p)
-    (== (cons a d) p)))
+(define (%member x l)
+  (conde
+    [(%null l) fail]
+    [(%car l x) succeed]
+    [else
+      (exist (d)
+        (%cdr l d)
+        (%member x d))]))
 
-(define %null
-  (lambda (x)
-    (== '() x)))
+(define (%rember x l out)
+  (conde
+    [(%null l) (%null out)]
+    [(%car l x) (%cdr l out)]
+    [else (exist (a d res)
+            (%cons a d l)
+            (%cons a res out)
+            (%rember x d res))]))
 
-(define %pair
-  (lambda (p)
-    (exist (a d)
-      (%cons a d p))))
-
-(define %list
-  (lambda (l)
-    (conde
-      ((%null l) succeed)
-      ((%pair l)
-       (exist (d)
-         (%cdr l d)
-         (%list d)))
-      (else fail))))
-
-(define %member
-  (lambda (x l)
-    (conde
-      ((%null l) fail)
-      ((%car l x) succeed)
-      (else
-        (exist (d)
-          (%cdr l d)
-          (%member x d))))))
-
-(define rembero  
-  (lambda (x l out)
-    (conde
-      ((%null l) (== '() out))
-      ((%car l x) (%cdr l out))
-      (else (exist (a d res)
-              (%cons a d l)
-              (rembero x d res)
-              (%cons a res out))))))
-
-(define %append
-  (lambda (l s out)
-    (conde
-      ((%null l) (== s out))
-      (else 
-        (exist (a d res)
-          (%cons a d l)
-          (%cons a res out)
-          (%append d s res))))))
+(define (%append l s out)
+  (conde
+    [(%null l) (== s out)]
+    [else 
+      (exist (a d res)
+        (%cons a d l)
+        (%cons a res out)
+        (%append d s res))]))
 
