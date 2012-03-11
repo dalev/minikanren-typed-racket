e015295a6141f5d81313557fc5e65e648acc71fb
Dale Vaillancourt <dalev@ccs.neu.edu>
2012-03-10 19:11:40 -0500

    reduce size

--- test/random-csv.rkt
+++ test/random-csv.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require (planet cce/fasttest/random))
 
-(for ([n-rows (in-range 0 1000000)])
+(for ([n-rows (in-range 0 500000)])
   (for ([n-columns (in-range 0 19)])
     (display (random-string #:len (random-natural/poisson 10)))
     (display #\,))
