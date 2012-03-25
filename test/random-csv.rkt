e9ab54d452d6658bb1ed8d272cb2df32a0d3f794
Dale Vaillancourt <dalev@ccs.neu.edu>
2012-03-25 12:31:28 -0400

    remove random csv

--- test/random-csv.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require (planet cce/fasttest/random))
-
-(for ([n-rows (in-range 0 500000)])
-  (for ([n-columns (in-range 0 19)])
-    (display (random-string #:len (random-natural/poisson 10)))
-    (display #\,))
-  (display (random-string))
-  (newline))
