56c6790a1e361531284a791712a33741650e6c77
Dale Vaillancourt <dalev@alum.wpi.edu>
2014-04-12 20:36:50 -0400

    remove planet dependency

--- test/count-csv.rkt
+++ test/count-csv.rkt
@@ -1,13 +1,10 @@
 #lang racket
 (require (prefix-in csv: "../lib/csv.rkt"))
-(require (prefix-in neil: (planet neil/csv:1:6/csv)))
-
-(define make-csv-reader (neil:make-csv-reader-maker '()))
 
 ;; About 80s in revision 359156f8400cf0236e87268b641e81ec1cbf2a35
 ;; on the 1 million row file
 (define (main input-file)
-  (displayln "Mine batched:")
+  (displayln "batched:")
   (call-with-input-file
     input-file
     (λ (in)
@@ -17,7 +14,7 @@
             (for/fold ([sum sum]) ([row (in-list rows)])
               (add1 sum)))))))
 
-  (displayln "Mine not batched:")
+  (displayln "not batched:")
   (call-with-input-file
     input-file
     (λ (in)
@@ -26,18 +23,7 @@
           (for/last ([rows (csv:in-raw in)]
                      [n (in-naturals 1)])
             n)))))
-
-  ;(displayln "Neil's:")
-  #;
-  (call-with-input-file
-    input-file
-    (λ (in)
-      (define next-row (make-csv-reader in))
-      (displayln 
-        (time 
-          (for/last ([_row (in-producer next-row '())]
-                     [n (in-naturals 1)])
-            n))))))
+  )
 
 (match (current-command-line-arguments)
   [(vector input) (main input)])
