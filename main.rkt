9d14ae40fe3ed05b253d4babd33e3eb8f97bc74d
Dale Vaillancourt <dalev@alum.wpi.edu>
2014-04-19 22:07:14 -0400

    simplify org

--- main.rkt
+++ main.rkt
@@ -1,9 +1,9 @@
 #lang racket/base
-(require "lib/main.rkt"
-         "lib/lib/list.rkt"
-         "lib/lib/sexp.rkt"
-         "lib/lib/util.rkt")
-(provide (all-from-out "lib/main.rkt"
-                       "lib/lib/list.rkt"
-                       "lib/lib/sexp.rkt"
-                       "lib/lib/util.rkt"))
+(require "lib/kanren.rkt"
+         "lib/list.rkt"
+         "lib/sexp.rkt"
+         "lib/util.rkt")
+(provide (all-from-out "lib/kanren.rkt"
+                       "lib/list.rkt"
+                       "lib/sexp.rkt"
+                       "lib/util.rkt"))
