5d2128c900b6c978613636dee07d548af74afaad
Dale Vaillancourt <dalev@alum.wpi.edu>
2014-04-19 21:56:41 -0400

    mv kanren -> lib

--- main.rkt
+++ main.rkt
@@ -1,9 +1,9 @@
 #lang racket/base
-(require "kanren/main.rkt"
-         "kanren/lib/list.rkt"
-         "kanren/lib/sexp.rkt"
-         "kanren/lib/util.rkt")
-(provide (all-from-out "kanren/main.rkt"
-                       "kanren/lib/list.rkt"
-                       "kanren/lib/sexp.rkt"
-                       "kanren/lib/util.rkt"))
+(require "lib/main.rkt"
+         "lib/lib/list.rkt"
+         "lib/lib/sexp.rkt"
+         "lib/lib/util.rkt")
+(provide (all-from-out "lib/main.rkt"
+                       "lib/lib/list.rkt"
+                       "lib/lib/sexp.rkt"
+                       "lib/lib/util.rkt"))
